352732e8ce5afa1c15c2b7d4ae6440ae201b4b5f
Chaitanya <cpatil2569@gmail.com>
2026-03-31 22:36:06 +0530

    updated app.js file

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express()
 const PORT = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello from Dockerized Node.js App 🚀')
+    res.send('Hello from Dockerized Node.js App 🚀 also, Hi my name is chaitanya and this is updated version ')
 })
 
 app.get('/about', (req, res) => {
